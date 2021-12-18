c8f87b433fc5e8f8855848e06fc0b24c02ef7659
vitorhugo13 <vhlg99@gmail.com>
2021-12-18 15:03:09 +0000

    Added exercise 4

--- Project/ex4.c
+++ Project/ex4.c
@@ -1,13 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <pthread.h>
+#include <math.h>
+#include <unistd.h>  
 
 #define P 5 //number of processes, which can be varied
+int thread_executions;
+int total_elems;
+float sum;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER; //allowing one thread in and blocking access to all others
 
-void launch_threads(int n_elems){
+void my_pthread_function(void* arg){
+
+    float aux_sum = 0;
+
+    if(total_elems == P){
+        printf("Thread %d executes sum %d\n", (int) arg + 1, (int) arg + 1);
+        aux_sum = aux_sum + ((1./ ((int) arg + 1)));
+    }
+    else if((float) total_elems / P == 0){ //if N(total_elems) is divisible by P
+        printf("Thread %d executes sum from %d to %d\n", (int) arg + 1, ((total_elems/P) * (int) arg) + 1, (total_elems/P) * ((int) arg + 1));
+        for(int i = ((total_elems/P) * (int) arg) + 1; i <= (total_elems/P) * ((int) arg + 1); i++){
+            aux_sum = aux_sum + ((1./ i));
+        }
+    }
+    else if(total_elems < P){ //the first nth threads execute 1 calculation
+        if((int) arg + 1 <= total_elems){
+            printf("Thread %d executes sum %d\n", (int) arg + 1, (int) arg + 1);
+            aux_sum = aux_sum + ((1./ ((int) arg + 1)));
+        }
+    }
+    else{ //otherwise
+        if((int) arg == P - 1){
+            printf("Thread %d executes sum from %d to %d\n", (int) arg + 1, ((total_elems/P) * (int) arg) + 1, total_elems);
+            for(int i = ((total_elems/P) * (int) arg) + 1; i <= total_elems; i++){
+                aux_sum = aux_sum + ((1./ i));
+            }
+        }
+        else{
+            printf("Thread %d executes sum from %d to %d\n", (int) arg + 1, ((total_elems/P) * (int) arg) + 1, (total_elems/P) * ((int) arg + 1));
+            for(int i = ((total_elems/P) * (int) arg) + 1; i <= (total_elems/P) * ((int) arg + 1); i++){
+                aux_sum = aux_sum + ((float) (1./ i));
+            }
+        }
+    }
+
+    pthread_mutex_lock(&mutex);
+    sum = sum + aux_sum;
+    pthread_mutex_unlock(&mutex);
+}
+
+void launch_threads(n_elems){
     pthread_t t_array[P]; //initialize array to store threads/processes
+    thread_executions = floor(n_elems/P); //number of executions each process, apart from the last one, will execute. The last thread is responsible for execution the "leftovers" when P doesn't divide N
 
     for(int i = 0; i < P; i++ ){
-        int thr = pthread_create( &t_array[i], NULL, my_pthread_function, i );
+        int thr = pthread_create(&t_array[i], NULL, my_pthread_function, i );
         if(thr) {
             printf( stderr, "Error creating thread."); 
             exit(-1);
@@ -17,10 +65,8 @@ void launch_threads(int n_elems){
     for(int i = 0; i < P; i++ ){
         pthread_join(t_array[i], NULL);
     }
-}
 
-void my_pthread_function(void* arg){
-
+    printf("Result: %f\n", sum - log(total_elems));
 }
 
 int main(int argc, char *argv[]){
@@ -34,6 +80,7 @@ int main(int argc, char *argv[]){
     else{
         sscanf(argv[1],"%d",&n_elems);
         printf("The number of elements of the sum: %d\n", n_elems);
+        total_elems = n_elems;
         launch_threads(n_elems);
     }
 
--- Project/report.md
+++ Project/report.md
@@ -2,4 +2,5 @@
 
 ## Exercise 4
 **Files required:** ```ex4.c```
-**Program Execution:**
+**Program Execution:** ```gcc ex4.c -o ex4 -lpthread -lm``` followed by ```./ex4 <number of elements of the sum>```
+**Example:** ```/ex4 50``` while P = 5
