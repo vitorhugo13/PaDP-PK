0c40ebe073c7bfa1a77dd747c796f75d52488bf1
vitorhugo13 <vhlg99@gmail.com>
2021-12-19 16:58:20 +0000

    Exercise 2 finished

--- Project/ex2.c
+++ Project/ex2.c
@@ -3,8 +3,9 @@
 #include <math.h>
 #include <omp.h>
 #include <pthread.h>
+#include "tmeas.h"
 
-#define THREADS 8
+int THREADS; //number of threads
 int n_subintervals; //number of elements of the sum
 int a = 0, b = 1;
 float h, sum = 0; 
@@ -79,6 +80,8 @@ void my_pthread_function(void* arg){
 void launch_threads(n_elems){
     pthread_t t_array[THREADS]; //initialize array to store threads/processes
 
+	tstart();
+
     for(int i = 0; i < THREADS; i++ ){
         int thr = pthread_create(&t_array[i], NULL, my_pthread_function, i );
         if(thr) {
@@ -93,23 +96,29 @@ void launch_threads(n_elems){
 
     sum = sum * h;
     sum = sum * 4; //because the initial integral is multiplied by 4
+
+    float time = tstop();
+
     printf("Result: %f\n", sum);
+    printf("Execution time: %f s\n", time);
     
 }
 
 int main(int argc, char *argv[]){
 
-    if(argc != 2){ //one parameter is missing or there are too many parameters
-        printf("To run the program use: ./<file> <number of subintervals of integration>\n");
+    if(argc != 3){ //one parameter is missing or there are too many parameters
+        printf("To run the program use: ./<file> <number of subintervals of integration> <number of threads>\n");
         return -1;
     }
     else{
         sscanf(argv[1],"%d",&n_subintervals);
+        sscanf(argv[2],"%d",&THREADS);
         if(n_subintervals < THREADS){
             printf("The number of subintervals of integration must be >= %d\n", THREADS);
             return -1;
         }
         printf("The number of subintervals of integration: %d\n", n_subintervals);
+        printf("The number of threads being used: %d\n", THREADS);
         h = ((b-a)/(float) n_subintervals);
         sum = (f(a) + f(b)) / (float) 2;
         launch_threads();
--- Project/report.md
+++ Project/report.md
@@ -1,5 +1,21 @@
 # PaDP project - Vítor Gonçalves
 
+## Exercise 2
+**Files required:** ```ex2.c```, ```tmeas.c``` and ```tmeas.h``` (these last two files were reused from laboratory classes to calculate the program's execution time.)
+
+**Program execution:** ```gcc ex2.c -o ex2 -lpthread -lm tmeas.c``` followed by ```./ex2 <number of subintervals of integration> <number of threads>```
+
+**Example:** ```./ex2 1000 10```
+
+![](https://i.imgur.com/zq2w16Q.png)
+
+**Comparing runtimes using different numbers of threads**
+* To study how the number of threads varies the execution time of programs, the program was executed using the same number of integration subintervals (1000), varying only the number of threads.
+* The result can be seen in the chart below. 
+
+![](https://i.imgur.com/GqSMfXv.png)
+Where the x_axis is the number of threads used, and the execution time, in seconds, is present in the y_axis.
+
 ## Exercise 4
 **Files required:** ```ex4.c```
 
